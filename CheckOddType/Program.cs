9f79205a27f35ebb46d8198a021e4c1643788d24
tomarin <tomarint@gmail.com>
2020-11-10 20:59:20 +0900

    Initial commit.

--- CheckOddType/Program.cs
+++ CheckOddType/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CheckOddType {
     class Program {
         static void Main(string[] args) {
+            dynamic master = Activator.CreateInstance(Type.GetTypeFromProgID("IMAPI2.MsftDiscMaster2"));
+            foreach (string uniqueId in master) {
+                dynamic recorder = Activator.CreateInstance(Type.GetTypeFromProgID("IMAPI2.MsftDiscRecorder2"));
+                recorder.InitializeDiscRecorder(uniqueId);
+                Console.WriteLine("{0}:", recorder.VolumePathNames[0]);
+                foreach (int id in recorder.SupportedFeaturePages)
+                    Console.WriteLine("    0x{0:X2}", id);
+            }
         }
     }
 }
